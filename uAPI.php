e46601c895db1dd6a065605f4e8faaeb7b0c99ce
K4CZP3R <k4czp3r.dev@gmail.com>
2017-02-03 19:24:56 +0100

    Remake of class
    changed file containing ticket to "api_ticket"

--- uAPI.php
+++ uAPI.php
@@ -1,267 +1,192 @@
 <?php
-
 /**
  * @author Kacper Serewis (k4czp3r.dev@gmail.com)
- * @copyright 2016
- * @version 1.0.0.0
- * 
- * Created 31-08-2016
+ * @copyright 2017
+ * @version 2.0.0.0
+ * Updated at 03-Feb-2017
  */
+class ubiapi{
+	private $b64authcreds;
+	public $http_useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
+	public $http_encoding="gzip";
+	public $debug=false;
 
-class uapi{
-    public $uplayemail;
-    public $uplaypassword;
-    public $debug;
-    public $useragent = "Mozilla/5.0 (Windows NT; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36";
-    
-    function __construct($email,$password,$debug=false){
-        $this->uplayemail = $email;
-        $this->uplaypassword = $password;
-        $this->debug = $debug;
-        $this->debugmsg("Defined<br>Mail:".$email."<br>Pass:YES<br>Debug:Yes<br>");
-    }
+	public function debugReport($content){
+		if($this->debug){
+			print date("h:i:s")." - ".$content."<br>";
+		}
+	}
+	public function generateB64Creds($emailandpassword){
+		$this->debugReport("B64: ".base64_encode($emailandpassword));
+		return base64_encode($emailandpassword);
+	}
+	function __construct($credsemail,$credspass,$credsb64){
+		if($credspass == null && $credsb64 != null){
+			$this->debugReport("Using b64 string to login");
+			$this->b64authcreds=$credsb64;
+		}
+		else{
+			$this->debugReport("Using creds to login");
+			$this->b64authcreds=$this->generateB64Creds($credsemail.":".$credspass);
+		}
+	}
+	public function searchUser($mode,$content,$returnRaw){
+		$prefixUrl = "https://api-ubiservices.ubi.com/v2/profiles?";
+		if($mode == 1 || $mode == "bynick"){
+			$request_url = $prefixUrl."nameOnPlatform=".$content."&platformType=uplay";
+		}
+		if($mode == 2 || $mode == "byid"){
+			$request_url = $prefixUrl."profileId=".$content;
+		}
+		$this->debugReport("Request URL: ".$request_url);
+		$request_header_ubiappid = "314d4fef-e568-454a-ae06-43e3bece12a6";
+		$request_header_ubisessionid = "a651a618-bead-4732-b929-4a9488a21d27";
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $request_url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$headers =[
+		"Accept: */*",
+		"ubi-appid: ".$request_header_ubiappid,
+		"ubi-sessionid: ".$request_header_ubisessionid,
+		"authorization: ".$this->uplayticket(false),
+		"Referer: https://club.ubisoft.com/en-US/friends",
+		"Accept-Language: en-US",
+		"Origin: https://club.ubisoft.com",
+		"Accept-Encoding: ".$this->http_encoding,
+		"User-Agent: ".$this->http_useragent,
+		"Host: api-ubiservices.ubi.com",
+		"Cache-Control: no-cache"];
 
-    public function changenickname($newNickname){
-        $newNickname = '{"nameOnPlatform":"'.$newNickname.'"}';
-        $newNicknameLen = strlen($newNickname);
-        $authStr = $this->getAuthStr();
-        if($authStr["error"]){
-            return array("error"=>true,
-            "error_msg"=>$authStr["error_msg"]);
-        }
-        $uplayId = $this->getId();
-        if($uplayId["error"]){
-            return array("error"=>true,
-            "error_msg"=>$authStr["error_msg"]);
-        }
-        $appid = "c5393f10-7ac7-4b4f-90fa-21f8f3451a04";
-        
-        
-        
-        $firsturl = "https://api-ubiservices.ubi.com/v2/profiles/".$uplayId["data"]."/validateUpdate";
-        $secondurl = $firsturl;
-        $finalurl = "https://api-ubiservices.ubi.com/v2/profiles/".$uplayId["data"];
-        $val1opts=array("http"=>array("method"=>"OPTIONS",
-        "header"=>"Host: api-ubiservices.ubi.com\r\n".
-        //"Connection: keep-alive\r\n".
-        "Access-Control-Request-Method: POST\r\n".
-        "Origin: https://account-uplay.ubi.com\r\n".
-        "User-Agent: ".$this->useragent."\r\n".
-        "Access-Control-Request-Heades: authorization, content-type, ubi-appid\r\n".
-        "Accept: */*\r\n".
-        "Referer: https://account-uplay.ubi.com/en-GB/account/information\r\n".
-        "Accept-Encoding: gzip, deflate, sdch, br\r\n".
-        "Accept-Language: nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4\r\n",));
-            
-        $val2opts=array("http"=>array("method"=>"POST",
-        "header"=>"Host: api-ubiservices.ubi.com\r\n".
-       // "Connection: keep-alive\r\n".
-        "Content-Lenght: ".$newNicknameLen."\r\n".
-        "authorization: ".$authStr["data"]."\r\n".
-        "ubi-appid: ".$appid."\r\n".
-        "Origin: https://account-uplay.ubi.com\r\n".
-        "User-Agent: ".$this->useragent."\r\n".
-        "content-type: application/json\r\n".
-        "Accept: */*\r\n".
-        "Referer: https://account-uplay.ubi.com/en-GB/account/information\r\n".
-        "Accept-Encoding: gzip, deflate, br\r\n".
-        "Accept-Language: nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4\r\n",
-        "content"=>$newNickname));
-            
-        $putopts=array("http"=>array("method"=>"PUT",
-        "header"=>"Host: api-ubiservices.ubi.com\r\n".
-       // "Connection: keep-alive\r\n".
-        "Content-Lenght: ".$newNicknameLen."\r\n".
-        "authorization: ".$authStr["data"]."\r\n".
-        "ubi-appid: ".$appid."\r\n".
-        "Origin: https://account-uplay.ubi.com\r\n".
-        "User-Agent: ".$this->useragent."\r\n".
-        "content-type: application/json\r\n".
-        "Accept: */*\r\n".
-        "Referer: https://account-uplay.ubi.com/en-GB/account/information\r\n".
-        "Accept-Encoding: gzip, deflate, br\r\n".
-        "Accept-Language: nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4\r\n",
-        "content"=>$newNickname));
-            
-        $firstcontext = stream_context_create($val1opts);
-        $secondcontext = stream_context_create($val2opts);
-        $finalcontext = stream_context_create($putopts);
-            
-        $FirstubiResp = file_get_contents($firsturl,false,$firstcontext);
-        $SecondubiResp = file_get_contents($secondurl,false,$secondcontext);
-        $FinalubiResp = file_get_contents($finalurl,false,$finalcontext);
-        if(empty($FinalubiResp)){
-            return array("error"=>true,
-            "error_msg"=>"Last request returned null",
-            "error_data"=>$FinalubiResp);
-        }
-        else{
-            $json = json_decode($FinalubiResp,true);
-            $ip = $json['clientIp'];
-            if(!empty($ip)){
-                return array("error"=>false,
-                "data"=>$ip,
-                "data_full"=>$FinalubiResp);
-            }
-            else{
-                return array("error"=>true,
-                "error_msg"=>"Bad username",
-                "error_data"=>$FinalubiResp);
-            }
-        }
-    }
-    public function simpleusersearch($mode,$string){
-        if($mode == 0){$geturl = "https://public-ubiservices.ubi.com/v2/profiles?platformType=uplay&nameOnPlatform=".$string;}
-        elseif($mode == 1){$geturl = "https://public-ubiservices.ubi.com/v2/profiles?profileId=".$string;}
-        else{return array("error"=>true,
-                "error_msg"=>"This mode doesn't exists for User Search");}
-        $appId = "412802ed-8163-4642-a931-8299f209fecb";
-        $authStr = $this->getAuthStr();
-        $getopts = array("http"=>array("method"=>"GET",
-        "header"=>"Host: uplayconnect.com\r\n" .
-        "Accept: application/json, text/plain, */*\r\n" .
-        "Ubi-AppId: ".$appId."\r\n" .
-        "Authorization: ".$authStr["data"]."\r\n".
-        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36\r\n".
-        "Referer: https://uplayoverlay.ubi.com/?appId=314d4fef-e568-454a-ae06-43e3bece12a6&genomeId=45366f4e-420c-4386-ac26-24b3d25f2a3b&loginType=popup&themeColor=%233F4A4C&height=40&leftTextDirection=true&locale=en-GB&minimizedOpacity=&parentUrl=https%3A%2F%2Fclub.ubi.com&userId=01e84770-c50e-402b-8132-b94016f93b77\r\n".
-        "Accept-Encoding: gzip, deflate\r\n".
-        "Accept-Language: nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4\r\n",));
-        $scontext = stream_context_create($getopts);
-        $ubiResp = file_get_contents($geturl,false,$scontext);
-        if(empty($ubiResp)){
-            return array("error"=>true,
-            "error_msg"=>"Response from ubi server is empty",
-            "error_data"=>$ubiResp);
-        }
-        else{
-            $ubirJson = json_decode($ubiResp,true);
-            $uNick = $ubirJson['profiles'][0]['nameOnPlatform'];
-            $uId = $ubirJson['profiles'][0]['profileId'];
-            if(empty($uNick) OR empty($uId)){
-                return array("error"=>true,
-                "error_msg"=>"User doesn't exist",
-                "error_data"=>$ubiResp);
-            }
-            if($mode == 0){return array("error"=>false,
-                    "data"=>$uId,
-                    "data_full"=>$ubiResp);}
-            elseif($mode == 1){return array("error"=>false,
-                    "data"=>$uNick,
-                    "data_full"=>$ubiResp);}
-        }
-        
-    }
-    public function login(){
-        $url = "https://uplayconnect.ubi.com/ubiservices/v2/profiles/sessions";
-        $appId = "c5393f10-7ac7-4b4f-90fa-21f8f3451a04";
-        $uEmail = $this->uplayemail;
-        $uPassword = $this->uplaypassword;
-        $bAuth = base64_encode($uEmail.":".$uPassword);
-        $postopts=array("http"=>array("method"=>"POST",
-        "header"=>"Host: uplayconnect.com\r\n".
-        "Connection: keep-alive\r\n".
-        "Content-Lenght: 2\r\n".
-        "Ubi-RequestedPlatformType: uplay\r\n".
-        "Origin: https://uplayconnect.ubi.com\r\n".
-        "Authorization: Basic ".$bAuth."\r\n".
-        "Content-Type: application/json; charset=UTF-8\r\n".
-        "Accept: */*\r\n".
-        "Ubi-AppId: ".$appId."\r\n".
-        "X-Requested-With: XMLHttpRequest\r\n".
-        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36\r\n".
-        "Referer: https://uplayconnect.ubi.com/Default/Login?appId=".$appId."&lang=en-GB\r\n".
-        "Accept-Encoding: gzip, deflate\r\n".
-        "Accept-Language: nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4\r\n",
-        "content"=>"{}"));
-        
-        $scontext = stream_context_create($postopts);
-        $ubiResp = file_get_contents($url,false,$scontext);
-        if(empty($ubiResp)){
-            return array("error"=>true,
-            "error_msg"=>"Ubi responded with empty string",
-            "error_data"=>$ubiResp);
-        }
-        else{
-            $ubirJson = json_decode($ubiResp,true);
-            $uTicket = $ubirJson['ticket'];
-            $uProfileId = $ubirJson['profileId'];
-            if(empty($uTicket)){
-                return array("error"=>true,
-                "error_msg"=>"Uplay Ticket is empty, see error_data for more",
-                "error_data"=>$ubiResp);
-            }
-            else{
-                if(empty($uProfileId)){
-                    return array("error"=>true,
-                    "error_msg"=>"Uplay ID is empty, see error_data for more",
-                    "error_data"=>$ubiResp);
-                }
-            
-                else{
-                    $this->writeAuthStr($uTicket);
-                    $this->writeId($uProfileId);
-                    return array("error"=>false,
-                    "data"=>$uTicket.":".$uProfileId,
-                    "data_full"=>$ubiResp);
-                }
-            }
-        }
-    }
-    private function debugmsg($str){
-        if($this->debug){
-            echo $str;
-        }
-    }
-    private function getId(){
-        $idF = fopen("ubi_id","r") or die("Error while opening profileid file");
-        $id = fgets($idF);
-        fclose($idF);
-        if($id == NULL){
-            return array("error"=>true,
-            "error_msg"=>"profileid is empty");
-        }
-        else{
-            return array("error"=>false,
-            "data"=>$id);
-        }
-    }
-    private function writeId($id){
-        $idF = fopen("ubi_id","w") or die("Can't open profile id");
-        try{
-            fwrite($idF,$id);
-            return array("error"=>false,
-            "data","ok");
-        }
-        catch(Exception $ex){
-            return array("error"=>true,
-            "error_msg"=>"Error while writitng to file (".$ex.")");
-        }
-    }
-    public function getAuthStr(){
-        $prefix = "Ubi_v1 t=";
-        $ticketF = fopen("ubi_authstr","r") or die("Error while opening ticket file");
-        $ticket = fgets($ticketF);
-        if($ticket == NULL){
-            return array("error"=>true,
-            "error_msg"=>"Ticket file is empty");
-        }
-        else{
-            return array("error"=>false,
-            "data"=>$prefix.$ticket);
-        }
-    }
-    private function writeAuthStr($valToWrite){
-        $ticketF = fopen("ubi_authstr","w") or die("Can't open ticket file");
-        try{
-            fwrite($ticketF,$valToWrite);
-            return array("error"=>false,
-            "data"=>"ok");
-        }
-        catch(Exception $ex){
-            return array("error"=>true,
-            "error_msg"=>"Error while writing to file  (".$ex.")");
-        }
-    }
-    
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$ubioutput = curl_exec($ch);
+		curl_close($ch);
+
+		if($this->http_encoding == "gzip"){
+			$ubioutput = gzdecode($ubioutput);
+		}
+		$this->debugReport("Executed request (to see it, uncomment log line)");
+		$this->debugReport($ubioutput);
+
+		//idk why but sometimes gzdecoded returns null
+		if(empty($ubioutput)){
+			$this->debugReport("After making use of ".$this->http_encoding. "decode, string is empty, using orginal one...");
+			$ubioutput=$orginaloutput;}
+		if($returnRaw){
+			return $ubioutput;
+		}
+		else{
+			return json_decode($ubioutput,true);
+		}
+
+	}
+	//if $returnRaw == false then it'll return array
+	public function getFriends($returnRaw){
+		$this->debugReport("Going to return friends array/json");
+		$request_url = "https://api-ubiservices.ubi.com/v2/profiles/me/friends?locale=en-US";
+		$request_header_ubiappid = "314d4fef-e568-454a-ae06-43e3bece12a6";
+		$request_header_ubisessionid= "a651a618-bead-4732-b929-4a9488a21d27"; //todo: check generation of it
+
+		$ch=curl_init();
+		curl_setopt($ch, CURLOPT_URL,$request_url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$headers=[
+		"Accept: */*",
+		"ubi-appid: ".$request_header_ubiappid,
+		"ubi-sessionid: ".$request_header_ubisessionid,
+		"authorization: ".$this->uplayticket(false),
+		"Referer: https://club.ubisoft.com/en-US/friends",
+		"Accept-Language: en-US",
+		"Origin: https://club.ubisoft.com",
+		"Accept-Encoding: ".$this->http_encoding,
+		"User-Agent: ".$this->http_useragent,
+		"Host: api-ubiservices.ubi.com",
+		"Cache-Control: no-cache"];
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$ubioutput=curl_exec($ch);
+		curl_close($ch);
+		//after testing i dont see reason to include gzdecoding here, if you want to, do it
+
+		$this->debugReport("Request Executed (to see it, uncomment next line)");
+		//$this->debugReport($ubioutput);
+
+		if($returnRaw){
+			return $ubioutput;
+		}
+		else{
+			return json_decode($ubioutput,true);
+		}
+		
+	}
+	public function login(){
+		$this->debugReport("Going to login...");
+		$request_url = "https://connect.ubi.com/ubiservices/v2/profiles/sessions";
+		$request_header_ubiappid="314d4fef-e568-454a-ae06-43e3bece12a6";
+		$request_header_authbasic=$this->b64authcreds;
+		$this->debugReport("<br>url:".$request_url."<br>appid:".$request_header_ubiappid."<br>authbasic:".$request_header_authbasic);
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $request_url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, '{"rememberMe":true}');
+		$headers = [
+		"Content-Type: application/json; charset=utf-8",
+		"Accept: */*",
+		"Ubi-AppId: ".$request_header_ubiappid,
+		"Ubi-RequestedPlatformType: uplay",
+		"Authorization: Basic ".$request_header_authbasic,
+		"X-Requested-With: XMLHttpRequest",
+		"Referer: https://connect.ubi.com/Default/Login?appId=".$request_header_ubiappid."&lang=en-US&nextUrl=https%3A%2F%2Fclub.ubisoft.com%2Flogged-in.html%3Flocale%3Den-US",
+		"Accept-Language: en-US",
+		"Accept-Encoding: ".$this->http_encoding,
+		"User-Agent: ".$this->http_useragent,
+		"Host: connect.ubi.com",
+		"Content-Lenght: 19", //change this when you are changing CURLOPT_POSTFIELDS
+		"Cache-Control: no-cache",
+		];
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$ubioutput = curl_exec($ch);
+		$orginaloutput=$ubioutput;
+		curl_close($ch);
+		if($this->http_encoding == "gzip"){
+			$ubioutput = gzdecode($ubioutput);
+		}
+		$this->debugReport("Executed request (to see it, uncomment log line)");
+		//$this->debugReport($ubioutput);
+
+		//idk why but sometimes gzdecoded returns null
+		if(empty($ubioutput)){
+			$this->debugReport("After making use of ".$this->http_encoding. "decode, string is empty, using orginal one...");
+			$ubioutput=$orginaloutput;}
+
+		$json = json_decode($ubioutput,true);
+		$this->debugReport("Your authstring (to see it, uncomment next line of code)");
+		//$this->debugReport($json['ticket']);
+		$this->debugReport("Welcome, ".$json['username']);
+		$this->debugReport("Your UserId is ".$json['userId']);
+		$this->debugReport("You can ignore last function for saving authstring but you'll need to edit some lines of codes to disable it");
+		$this->uplayticket(true,$json['ticket']);
+
+	}
+	public function uplayticket($save,$ticket=""){
+		if($save){
+			$this->debugReport("Saving ticket...");
+			$file_ticket = fopen("api_ticket","w") or die("Can't open ticket file");
+			try{
+				fwrite($file_ticket, $ticket);
+				return true;
+			}
+			catch(Exception $e){
+				return false;
+			}
+		}
+		else{
+			$this->debugReport("Gonna return formated ticket");
+			$prefix = "Ubi_v1 t=";
+			$ticket_file = fopen("api_ticket","r") or die("Can't open ticket file");
+			$ticket = fgets($ticket_file);
+			return $prefix.$ticket;
+		}
+	}
+	
 }
-?>
